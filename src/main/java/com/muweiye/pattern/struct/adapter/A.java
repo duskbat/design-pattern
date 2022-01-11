03cf14388f4d5db9591e11917eed4aa144f8b33b
muweiye <weiye.mu@synapsor.com>
2022-01-11 21:05:47 +0800

    adaptor

---
+++ src/main/java/com/muweiye/pattern/struct/adapter/A.java
@@ -0,0 +1,9 @@
+package com.muweiye.pattern.struct.adapter;
+
+/**
+ * 目标接口
+ */
+public interface A {
+
+    void afunc();
+}
---
+++ src/main/java/com/muweiye/pattern/struct/adapter/B.java
@@ -0,0 +1,6 @@
+package com.muweiye.pattern.struct.adapter;
+
+public interface B {
+
+    void bfunc();
+}
