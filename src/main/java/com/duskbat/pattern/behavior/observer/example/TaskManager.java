c00526f04ef256bd86a6600bfa5daa4ceba9d90a
muweiye <muwy1995@outlook.com>
2022-02-22 20:12:36 +0800

    observer example

--- src/main/java/com/duskbat/pattern/behavior/observer/example/TaskManager.java
+++ src/main/java/com/duskbat/pattern/behavior/observer/example/TaskManager.java
@@ -1,11 +1,13 @@
 package com.duskbat.pattern.behavior.observer.example;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class TaskManager implements TaskStatusChangedObserver {
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void actionTaskStatusChange(Object param) throws RuntimeException {
         System.out.println("do some transaction");
     }
